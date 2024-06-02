805b3c85333085ae6156e45b65eece2d67a50581
Maxwel <maxwelfcoelho@gmail.com>
2024-06-02 20:27:48 -0300

    refactor: refactor the product scraper to not fail, if the website amazon changes the structure, or it has a malformed structure

--- server/src/services/scrape-amazon-products.js
+++ server/src/services/scrape-amazon-products.js
@@ -10,15 +10,47 @@ function scrapeProducts(html) {
 
     const products = [];
     contentDiv.forEach((productDiv) => {
-        const productImageUrl = productDiv.querySelector("[data-image-load]").getAttribute("src");
-        const productTitle = productDiv.querySelector('[data-cy="title-recipe"]').children[0].textContent.trim();
-        const productRating = productDiv.querySelector('.a-icon-alt').textContent;
-        const productReviews = parseInt(productDiv.querySelector('[data-component-type="s-client-side-analytics"]').textContent);
+        const productImageUrl = scrapeProductImage(productDiv);
+        const productTitle = scrapeProductTitle(productDiv);
+        const productRating = scrapeProductRating(productDiv);
+        const productReviews = scrapeProductReviews(productDiv);
 
         const product = new Product(productTitle, productRating, productReviews, productImageUrl);
         products.push(product);
     });
     return products;
+}
+
+function scrapeProductReviews(productDiv) {
+    const productReviews = productDiv.querySelector('[data-component-type="s-client-side-analytics"]')
+    if (!productReviews) {
+        return 0;
+    }
+    return isNaN(parseInt(productReviews.textContent)) ? 0 : parseInt(productReviews.textContent);
+}
+
+function scrapeProductRating(productDiv) {
+    const productRating = productDiv.querySelector('.a-icon-alt')
+    if (!productRating) {
+        return '';
+    }
+    return productRating.textContent;
+}
+
+function scrapeProductTitle(productDiv) {
+    const productTitle = productDiv.querySelector('[data-cy="title-recipe"]')
+    if (!productTitle || productTitle.children.length === 0) {
+        return  '';
+    }
+    return productTitle.children[0].textContent.trim();
+}
+
+function scrapeProductImage(productDiv) {
+    const productImageUrl = productDiv.querySelector("[data-image-load]");
+    if (!productImageUrl) {
+        return '';
+    }
+    return productImageUrl.getAttribute("src");
 }
 
 module.exports = scrapeProducts;
