bdc058eca873e539e3e338efcac4fa6e6858114a
Maxwel <maxwelfcoelho@gmail.com>
2024-06-03 15:41:02 -0300

    chore: add cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const scrapeRouter = require("./routes/scrape");
 
 const app = express();
 
+app.use(cors());
 app.use(scrapeRouter);
 
 app.listen(4000);
